694fe44c8711818b250a359032a8095024cb2f9c
Alexis Gonzalez <1991.alexis@gmail.com>
2023-01-21 13:32:28 -0400

    Created pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonComponent } from './components/layout/button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { NavigationComponent } from './components/layout/navigation/navigation.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { CardComponent } from './components/card/card.component';
+import { WelcomePage } from './pages/welcome/welcome.page';
+import { UserPage } from './pages/user/user.page';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    NavigationComponent
+    NavigationComponent,
+    CardComponent,
+    WelcomePage,
+    UserPage
   ],
   imports: [
     BrowserModule,
